6cb1534600dcd4c982771deb20c94d58744f54ae
rockethelll <51137055+rockethelll@users.noreply.github.com>
2025-11-25 10:15:48 +0000

    Build: (d3915f5) Refactor app layout by replacing PDF iframe with an image for the BAFA section, and adjust text styling for improved readability.

--- edln-caen/server/angular-app-manifest.mjs
+++ edln-caen/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 7418, hash: '0ed4987802b05e55ab5abc16db882fbd037b588a55f6e2563f414f7b593c5cde', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 3387, hash: '0f468b235296431c1a745a68456d773d997cf346b3cdd64225772daa827d82a3', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 34301, hash: 'db3874935fbcdf2c036eeae7e735d82363cef0f5668a2e4c25362ea55af760a3', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'styles-NCWWEV3E.css': {size: 13792, hash: 's9Xt4BF3Pi0', text: () => import('./assets-chunks/styles-NCWWEV3E_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 7472, hash: '40b6bf8d0fbb1d483ee560796186394cf2e9478daebb13a4199518bdbd39ae58', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 3387, hash: 'b19d52a00207f6f0a42cc0d0cce64e7a63befbbf39f813e055fe874e663e8c8d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 34379, hash: '2e7770c28b95e5447cc10aa7ad7541b44125847665f0750c9ec1beb899e33c68', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-6UMBY77A.css': {size: 13976, hash: 'uyZAoXGenN8', text: () => import('./assets-chunks/styles-6UMBY77A_css.mjs').then(m => m.default)}
   },
 };
